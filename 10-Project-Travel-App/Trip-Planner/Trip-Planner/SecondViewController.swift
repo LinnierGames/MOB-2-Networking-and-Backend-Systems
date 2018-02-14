9397f300f64f3d68e46ff759467e6ae046d5d626
LinnierGames <esericksanc@gmail.com>
2018-02-13 19:18:54 -0800

    init xcode project

---
+++ 10-Project-Travel-App/Trip-Planner/Trip-Planner/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Trip-Planner
+//
+//  Created by Erick Sanchez on 2/13/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
